19b334792bdfa4f132ec4d19b5364b72084db11e
achamas811 <alexandra.chamas@stonybrook.edu>
2025-04-05 20:47:50 -0400

    Added change tabs between home and about

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+var home = document.getElementById('home');
+var homePage = document.getElementById('homePage');
+var about = document.getElementById('about');
 var aboutMe = document.getElementById('aboutMe');
 
 window.onload = function () {
+    homePage.style.display = 'block';
     aboutMe.style.display = 'none';
+    home.addEventListener('click', () => {
+        homePage.style.display = 'block';
+        aboutMe.style.display = 'none';
+    })
+    about.addEventListener('click', () =>{
+        homePage.style.display = 'none';
+        aboutMe.style.display = 'block';
+    })
+
+    
 }
