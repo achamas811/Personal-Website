07612eba9c0d8ad4ebd8de49a8f2d68b5177ac0d
achamas811 <alexandra.chamas@stonybrook.edu>
2025-04-06 15:50:48 -0400

    Projects Page Part 1

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ var home = document.getElementById('home');
 var homePage = document.getElementById('homePage');
 var about = document.getElementById('about');
 var aboutMe = document.getElementById('aboutMe');
+var projects = document.getElementById('projects');
+var projectPage = document.getElementById('projectPage');
 
 window.onload = function () {
     homePage.style.display = 'block';
     aboutMe.style.display = 'none';
+    projectPage.style.display = 'none';
+
     home.addEventListener('click', () => {
         homePage.style.display = 'block';
         aboutMe.style.display = 'none';
+        projectPage.style.display = 'none';
     })
     about.addEventListener('click', () =>{
         homePage.style.display = 'none';
         aboutMe.style.display = 'block';
+        projectPage.style.display = 'none';
+    })
+    projects.addEventListener('click', () => {
+        homePage.style.display = 'none';
+        aboutMe.style.display = 'none';
+        projectPage.style.display = 'block';
     })
 
     
