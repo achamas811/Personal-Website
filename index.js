c11f361468301a1d820df5debdee8449b293493f
achamas811 <alexandra.chamas@stonybrook.edu>
2025-04-06 19:43:25 -0400

    trouble shooting part 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var home = document.getElementById('home');
+var name_ = document.getElementById('name');
 var homePage = document.getElementById('homePage');
 var about = document.getElementById('about');
 var aboutMe = document.getElementById('aboutMe');
@@ -7,21 +8,31 @@ var projectPage = document.getElementById('projectPage');
 
 window.onload = function () {
     homePage.style.display = 'block';
+    name_.style.display= 'block';
     aboutMe.style.display = 'none';
     projectPage.style.display = 'none';
 
     home.addEventListener('click', () => {
         homePage.style.display = 'block';
+        name_.style.display = 'block';
+        aboutMe.style.display = 'none';
+        projectPage.style.display = 'none';
+    })
+    name_.addEventListener('click', () => {
+        homePage.style.display = 'block';
+        name_.style.display = 'block';
         aboutMe.style.display = 'none';
         projectPage.style.display = 'none';
     })
     about.addEventListener('click', () =>{
         homePage.style.display = 'none';
+        name_.style.display = 'block';
         aboutMe.style.display = 'block';
         projectPage.style.display = 'none';
     })
     projects.addEventListener('click', () => {
         homePage.style.display = 'none';
+        name_.style.display = 'block';
         aboutMe.style.display = 'none';
         projectPage.style.display = 'block';
     })
