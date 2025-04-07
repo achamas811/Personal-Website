0e8661ec8bfb3398b0584e8cb724c5d21174489f
achamas811 <alexandra.chamas@stonybrook.edu>
2025-04-06 20:42:52 -0400

    Started the contact me page

--- index.js
+++ index.js
@@ -5,36 +5,45 @@ var about = document.getElementById('about');
 var aboutMe = document.getElementById('aboutMe');
 var projects = document.getElementById('projects');
 var projectPage = document.getElementById('projectPage');
+var contact = document.getElementById('contact');
+var contactMe = document.getElementById('contactMe');
 
 window.onload = function () {
     homePage.style.display = 'block';
-    name_.style.display= 'block';
     aboutMe.style.display = 'none';
     projectPage.style.display = 'none';
+    contactMe.style.display = 'none';
+
 
     home.addEventListener('click', () => {
         homePage.style.display = 'block';
-        name_.style.display = 'block';
         aboutMe.style.display = 'none';
         projectPage.style.display = 'none';
+        contactMe.style.display = 'none';
     })
     name_.addEventListener('click', () => {
         homePage.style.display = 'block';
-        name_.style.display = 'block';
         aboutMe.style.display = 'none';
         projectPage.style.display = 'none';
+        contactMe.style.display = 'none';
     })
     about.addEventListener('click', () =>{
         homePage.style.display = 'none';
-        name_.style.display = 'block';
         aboutMe.style.display = 'block';
         projectPage.style.display = 'none';
+        contactMe.style.display = 'none';
     })
     projects.addEventListener('click', () => {
         homePage.style.display = 'none';
-        name_.style.display = 'block';
         aboutMe.style.display = 'none';
         projectPage.style.display = 'block';
+        contactMe.style.display = 'none';
+    })
+    contact.addEventListener('click', () => {
+        homePage.style.display = 'none';
+        aboutMe.style.display = 'none';
+        projectPage.style.display = 'none';
+        contactMe.style.display = 'block';
     })
 
     
